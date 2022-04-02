6dbae8f645c2f93acc70793e09ec2f1cc0621661
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-02 14:10:21 +0300

    change logic

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const todo = ({ todo }) => {
     const { id, title, completed } = todo;
+    const h1 = <h1>{title}</h1>;
+    const text = completed ? <strike>{h1}</strike> : h1;
 
     return (
         <div data-testid='todo-1' key={id}>
-            {title}: {completed ? 'done' : 'waiting'}
+            {text}
         </div>
     );
 };
