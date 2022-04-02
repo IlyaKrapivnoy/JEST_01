58498acdcec5cb8fee41138b97ebff8b2cc77875
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-02 14:13:22 +0300

    render non-completed todo

--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -1,9 +1,10 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import Todo from '../todo';
 
-test('should render todo component', () => {
-    render(<Todo />);
+test('should render non-completed todo item', () => {
+    const todo = { id: 1, title: 'wash dishes', completed: false };
+    render(<Todo todo={todo} />);
     const todoElement = screen.getByTestId('todo-1');
     expect(todoElement).toBeInTheDocument();
-    expect(todoElement).toHaveTextContent('Hello world');
+    expect(todoElement).toHaveTextContent('wash dishes');
 });
