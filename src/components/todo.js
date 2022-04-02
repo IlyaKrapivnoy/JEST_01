d61642573e5117c6bbb3fc9bf48ce4d0f88cda17
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-02 14:37:48 +0300

    upd snapshot

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const todo = ({ todo }) => {
     const { id, title, completed } = todo;
-    const h1 = <h1>{title}</h1>;
+    const h1 = <h1>todo - {title}</h1>;
     const text = completed ? <s>{h1}</s> : h1;
 
     return (
-        <div data-testid='todo-1' key={id}>
+        <div data-testid='todo-1' key={`todo-${id}`}>
             {text}
         </div>
     );
